66f89171e24f09dc6536e8bf916d6055b49ac172
Alex Nichol <unixpickle@gmail.com>
2015-05-15 12:43:31 -0400

    fix up whitespace

--- src/cube_design.js
+++ src/cube_design.js
@@ -1,5 +1,5 @@
 (function() {
-  
+
   var DEFAULT_CUBE_DESIGN = {
     edgeWidth: 1,
     edgeHeight: 1,
@@ -12,14 +12,14 @@
     centerSize: 1,
     centerRadius: 0.1
   };
-  
+
   var DESIGN_ATTRIBUTES = Object.keys(DEFAULT_CUBE_DESIGN);
-  
+
   function CubeDesign(attrs) {
     for (var i = 0, len = DESIGN_ATTRIBUTES.length; i < len; ++i) {
       var attr = DESIGN_ATTRIBUTES[i];
       this[attr] = (attrs[attr] || DEFAULT_CUBE_DESIGN[attr]);
     }
   }
-  
+
 })();
